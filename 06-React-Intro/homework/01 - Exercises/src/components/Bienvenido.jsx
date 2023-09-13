d5a939c7e64cc7595934f8378edb4829b4ca2836
Deras879 <derycard59@gmail.com>
2023-09-13 18:40:18 -0500

    Finalizado HW React intro

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Botones from "./Botones"
 
-const studentName = "";
+const studentName = "Deras";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return(
+    <div>
+      <h1>Mi proyecto de React</h1>
+      <h3>{studentName}</h3>
+      <ol>
+          {techSkills.map((skill => {
+            return <li>{skill}</li>
+          }))}
+      </ol>
+      <Botones m1={alerts.m1} m2={alerts.m2}/>
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
